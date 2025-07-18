b38dc2c238d5344c07a27ffe427b643342f3ae29
Urvish patel <patelurvish0706@gmail.com>
2025-07-19 01:05:42 +0530

    v0.5

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -32,6 +32,7 @@ $conn->close();
 <html lang="en">
 
 <head>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚕️</text></svg>">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Portal 👤 - PIMS</title>
--- dashboard_user.php
+++ dashboard_user.php
@@ -25,6 +25,7 @@ $user = $result->fetch_assoc();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Portal ❤️ - PIMS</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚕️</text></svg>">
     <link rel="stylesheet" href="style/navbar.css">
     <link rel="stylesheet" href="style/form.css">
     <link rel="stylesheet" href="style/style.css">
